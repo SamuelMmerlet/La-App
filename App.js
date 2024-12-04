6a386c101080654e70c7ff1ee3d423a9a8aae0e7
SamuelMmerlet <samuel.matus2024@merlet.superate.org.sv>
2024-12-03 20:29:37 -0600

    fkit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import HomeScreen from './screen/HomeScreen';
+import RecommendationsScreen from './screen/Recomendaciones';
+import AboutUsScreen from './screen/Aboutus';
+import FlightBookingScreen from './screen/Booking';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [currentScreen, setCurrentScreen] = useState('Home');
+
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case 'Home':
+        return <HomeScreen navigate={setCurrentScreen} />;
+      case 'Recommendations':
+        return <RecommendationsScreen navigate={setCurrentScreen} />;
+      case 'AboutUs':
+        return <AboutUsScreen navigate={setCurrentScreen} />;
+      case 'FlightBooking':
+        return <FlightBookingScreen navigate={setCurrentScreen} />;
+      default:
+        return <HomeScreen navigate={setCurrentScreen} />;
+    }
+  };
+
+  return <View style={styles.container}>{renderScreen()}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
